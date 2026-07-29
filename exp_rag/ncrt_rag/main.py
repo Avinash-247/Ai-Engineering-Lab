9b859eb5928f3806c7548f023fa4b82c3e2936fc
Avinash-247 <vempatistalin@gmail.com>
2026-07-29 18:16:05 +0530

    rag_ncet

--- exp_rag/ncrt_rag/main.py
+++ exp_rag/ncrt_rag/main.py
@@ -11,4 +11,17 @@ load_dotenv()
 
 embeddings = GoogleGenerativeAIEmbeddings(model="text-embedding-005")
 vector = embeddings.embed_query("hello, world!")
-print(vector[:5])'''
+print(vector[:5])
+import os
+print(os.getenv("GOOGLE_API_KEY"))
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0
+)
+
+response = llm.invoke("Say hello")
+
+print(response.content)'''
--- exp_rag/ncrt_rag/ncrt.py
+++ exp_rag/ncrt_rag/ncrt.py
@@ -4,7 +4,9 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
 from langchain_classic.indexes import SQLRecordManager
 from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
 
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 loader =PyPDFLoader("./knowledge/ncrt_book.pdf")
@@ -22,9 +24,8 @@ print(len(chunks))
 print(chunks[0].metadata)
 
 embedding=GoogleGenerativeAIEmbeddings(model="text-embedding-005")
-vector=embedding.embed_documents(
-    [chunk.page_content for chunk in chunks]
-    )
+#vector=embedding.embed_documents(
+   # [chunk.page_content for chunk in chunks])
 #print(vector)
 vector_store=Chroma.from_documents(
     collection_name="ncrt_book",
@@ -36,6 +37,58 @@ print(vector_store)
 
 record_manager=SQLRecordManager(
     namespace="ncrt_book",
-    db_url="sql:///record_manager.sql",
-    vector_store=vector_store
+    db_url="sqlite:///record_manager.sql",
 )
+
+retriver=vector_store.as_retriever(
+    search_kwargs={"k":4}
+)
+
+results=retriver.invoke(
+    "what is Arithmetic Expressions ?"
+)
+
+for doc in results:
+    print("="*50)
+    print(doc.page_content)
+    print(doc.metadata)
+
+llm=ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0
+)
+
+
+prompt = ChatPromptTemplate.from_template("""
+You are an expert NCERT teacher.
+
+Answer ONLY from the provided context.
+
+If the answer is not present in the context,
+say:
+
+"I couldn't find this information in the NCERT book."
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:
+""")
+
+context="\n\n".join(
+    doc.page_content for doc in results
+)
+
+final_prompt=prompt.invoke({
+    "context":context,
+    "question":"what is Arithmetic Expressions"
+    }
+)
+
+response=llm.invoke(final_prompt)
+
+print(response.content)
+
