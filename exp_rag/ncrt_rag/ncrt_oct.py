8b6460fdc1799d4f1f743ba79b70f40345242528
Avinash-247 <vempatistalin@gmail.com>
2026-08-03 18:59:32 +0530

    embedding creating

--- exp_rag/ncrt_rag/ncrt_oct.py
+++ exp_rag/ncrt_rag/ncrt_oct.py
@@ -8,6 +8,9 @@ import numpy as np
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
 doc=fitz.open("knowledge/ncrt_book.pdf")
 output_folder="knowledge/extracted_images"
 os.makedirs(output_folder, exist_ok=True)
@@ -60,94 +63,15 @@ for page_numbers in range(len(document)):
 
     print(f"saved:{text_file_name}")
 
+embedding=GoogleGenerativeAIEmbeddings(model="text-embedding-005")
 
-'''for page_number in range(len(doc)):
-    page=doc[page_number]
-    images=page.get_images()
+persist_directory="knowledge2/chroma_db"
 
-    for image_index,image in enumerate(images):
-        xref=image[0]
-        image_data=doc.extract_image(xref)
+vectorstore=Chroma.from_documents(
+    document=chunk,
+    embedding=embedding,
+    persist_directory=persist_directory,
+    collection_name="ncrt_ocr"
+)
 
-        image_bytes=image_data['image']
-        image_extension=image_data['ext']
-
-        filename=(
-            f"page{page_number +1:03d}"
-            f"image{image_index+1:03d}"
-            f".{image_extension}"
-
-        )
-                         
-        image_path=os.path.join(output_folder,filename)
-        with open(image_path, "wb") as file:
-            file.write(image_bytes)
-
-        print(f"saved: {filename}")
-
-doc.close()'''
-
-
-
-
-
-
-'''import fitz
-import os 
-
-doc=fitz.open("knowledge/ncrt_book.pdf")
-page=doc[0]
-images = page.get_images()
-xref=images[0][0]
-
-image_data =doc.extract_image(xref)
-
-page=doc[0]
-images=page.get_images()
-for image in images:
-    print(image[0])
-
-xref=images[0][0]
-image_data =doc.extract_image(xref)
-
-print(type(image_data))
-print(image_data.keys())
-print(images)
-
-output_folder="knowledge/extracted_images"
-os.makedirs(output_folder, exist_ok=True)
-image_bytes=image_data["image"]
-image_extension=image_data['ext']
-filename=f"page1.{image_extension}"
-
-image_path=os.path.join(output_folder,filename)
-with open(image_path, "wb") as file:
-    file.write(image_bytes)
-print("save", image_path)
-output_folder = "knowledge/extracted_images"
-#extracting all pdf images
-for page_numbers in range(len(doc)):
-    page=doc[page_numbers]
-    images = page.get_images()
-
-    #print(f"page {page_numbers +1} has {len(images)} images")
-
-    for image_index,image in enumerate(images):
-        xref=image[0]
-
-        image_data=doc.extract_image(xref)
-
-        image_bytes=image_data["images"]
-        image_extension=image_data["ext"]
-
-        filename=(
-            f"page{page_numbers + 1:03d}"
-            f"image{image_index+1:03d}"
-            f".{image_extension}"
-        )
-
-        image_path=os.path.join(output_folder,filename)
-        with open(image_path,"wb") as file:
-            file.write(image_bytes)
-
-        print(f"saved:{filename}")'''
+print(len(chunk),persist_directory)
